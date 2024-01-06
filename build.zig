c60db9cd66473f0e9bc2669d5b179baa808eb387
arctic-marmoset <45583929+arctic-marmoset@users.noreply.github.com>
2024-01-06 17:24:43 -0500

    Fix build for Zig version 0.12.0-dev.2059+42389cb9c

--- build.zig
+++ build.zig
@@ -9,20 +9,20 @@ pub fn build(b: *std.Build) !void {
     const optimize = b.standardOptimizeOption(.{});
 
     const kernel_interface = b.createModule(.{
-        .source_file = .{ .path = "kernel/kernel.zig" },
+        .root_source_file = .{ .path = "kernel/kernel.zig" },
     });
 
     const bootloader = b.addExecutable(.{
         .name = "bootx64",
         .root_source_file = .{ .path = "bootloader/main.zig" },
-        .target = .{
+        .target = std.Build.resolveTargetQuery(b, .{
             .cpu_arch = .x86_64,
             .os_tag = .uefi,
             .abi = .msvc,
-        },
+        }),
         .optimize = optimize,
     });
-    bootloader.addModule("kernel", kernel_interface);
+    bootloader.root_module.addImport("kernel", kernel_interface);
 
     const install_ovmf_vars = b.addInstallFile(.{ .path = "vendor/edk2-ovmf/x64/OVMF_VARS.fd" }, "OVMF_VARS.fd");
     bootloader.step.dependOn(&install_ovmf_vars.step);
@@ -35,24 +35,24 @@ pub fn build(b: *std.Build) !void {
     const kernel = b.addExecutable(.{
         .name = "kernel",
         .root_source_file = .{ .path = "kernel/main.zig" },
-        .target = .{
+        .target = std.Build.resolveTargetQuery(b, .{
             .cpu_arch = .x86_64,
             .os_tag = .freestanding,
             .abi = .none,
-        },
+        }),
         .optimize = optimize,
     });
     kernel.entry = .{ .symbol_name = kernel_entry_name };
-    kernel.red_zone = false;
-    kernel.addOptions("config", kernel_config);
+    kernel.root_module.red_zone = false;
+    kernel.root_module.addOptions("config", kernel_config);
 
     const make_hdd_structure_step = b.step("hdd", "Make HDD directory structure");
-    const copy_bootloader = b.addInstallFile(bootloader.getOutputSource(), "hdd/EFI/BOOT/bootx64.efi");
-    if (bootloader.optimize != .ReleaseSmall) {
-        const copy_bootloader_debug_symbols = b.addInstallFile(bootloader.getOutputPdbSource(), "hdd/EFI/BOOT/bootx64.pdb");
+    const copy_bootloader = b.addInstallFile(bootloader.getEmittedBin(), "hdd/EFI/BOOT/bootx64.efi");
+    if (optimize != .ReleaseSmall) {
+        const copy_bootloader_debug_symbols = b.addInstallFile(bootloader.getEmittedPdb(), "hdd/EFI/BOOT/bootx64.pdb");
         make_hdd_structure_step.dependOn(&copy_bootloader_debug_symbols.step);
     }
-    const copy_kernel = b.addInstallFile(kernel.getOutputSource(), "hdd/kernel.elf");
+    const copy_kernel = b.addInstallFile(kernel.getEmittedBin(), "hdd/kernel.elf");
     make_hdd_structure_step.dependOn(&copy_bootloader.step);
     make_hdd_structure_step.dependOn(&copy_kernel.step);
     b.default_step = make_hdd_structure_step;
