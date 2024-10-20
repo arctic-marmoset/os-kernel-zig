f01cd8761e16c4f0845608fe4664821ae822a974
arctic-marmoset <45583929+arctic-marmoset@users.noreply.github.com>
2024-10-20 00:53:21 -0400

    Factor out page table deep copy

--- kernel/paging.zig
+++ kernel/paging.zig
@@ -11,88 +11,102 @@ pub fn init() !void {
         pmm.physicalToVirtual(page_table_address).value,
     );
 
-    const new_page_table: *align(std.mem.page_size) native.PML4 = @ptrCast(
-        try pmm.allocPage(),
-    );
-
-    new_page_table.* = .{};
-    for (page_table.entries, &new_page_table.entries) |pml4e, *new_pml4e| {
-        if (pml4e.present) {
-            new_pml4e.* = pml4e;
-            const pdpt_address = pml4e.getAddress();
-
-            const pdpt: *align(std.mem.page_size) native.PDPT = @ptrFromInt(
-                pmm.physicalToVirtual(pdpt_address).value,
-            );
-
-            const new_pdpt: *align(std.mem.page_size) native.PDPT = @ptrCast(
-                try pmm.allocPage(),
-            );
-            new_pml4e.setAddress(
-                pmm.virtualToPhysical(.{
-                    .value = @intFromPtr(new_pdpt),
-                }),
-            );
-
-            new_pdpt.* = .{};
-            for (pdpt.entries, &new_pdpt.entries) |pdpte, *new_pdpte| {
-                if (pdpte.present) {
-                    new_pdpte.* = pdpte;
-                    if (pdpte.leaf) {
-                        continue;
-                    }
-
-                    const pdt_address = pdpte.getTableAddress();
-
-                    const pdt: *align(std.mem.page_size) native.PDT = @ptrFromInt(
-                        pmm.physicalToVirtual(pdt_address).value,
-                    );
-
-                    const new_pdt: *align(std.mem.page_size) native.PDT = @ptrCast(
-                        try pmm.allocPage(),
-                    );
-                    new_pdpte.setTableAddress(
-                        pmm.virtualToPhysical(.{
-                            .value = @intFromPtr(new_pdt),
-                        }),
-                    );
-
-                    new_pdt.* = .{};
-                    for (pdt.entries, &new_pdt.entries) |pdte, *new_pdte| {
-                        if (pdte.present) {
-                            new_pdte.* = pdte;
-                            if (pdte.leaf) {
-                                continue;
-                            }
-
-                            const pt_address = pdte.getTableAddress();
-
-                            const pt: *align(std.mem.page_size) native.PT = @ptrFromInt(
-                                pmm.physicalToVirtual(pt_address).value,
-                            );
-
-                            const new_pt: *align(std.mem.page_size) native.PT = @ptrCast(
-                                try pmm.allocPage(),
-                            );
-                            new_pdte.setTableAddress(
-                                pmm.virtualToPhysical(.{
-                                    .value = @intFromPtr(new_pt),
-                                }),
-                            );
-
-                            @memcpy(&new_pt.entries, &pt.entries);
-                        }
-                    }
-                }
-            }
-        }
-    }
+    const new_page_table = try deepCopyPageTable(page_table);
 
     setPageTableAddress(
         pmm.virtualToPhysical(.{
             .value = @intFromPtr(new_page_table),
         }),
     );
+}
+
+pub fn deepCopyPageTable(
+    page_table: *align(std.mem.page_size) native.PML4,
+) !*align(std.mem.page_size) native.PML4 {
+    const new_page_table: *align(std.mem.page_size) native.PML4 = @ptrCast(
+        try pmm.allocPage(),
+    );
+
+    new_page_table.* = .{};
+    for (page_table.entries, &new_page_table.entries) |pml4e, *new_pml4e| {
+        if (!pml4e.present) {
+            continue;
+        }
+
+        new_pml4e.* = pml4e;
+        const pdpt_address = pml4e.getAddress();
+
+        const pdpt: *align(std.mem.page_size) native.PDPT = @ptrFromInt(
+            pmm.physicalToVirtual(pdpt_address).value,
+        );
+
+        const new_pdpt: *align(std.mem.page_size) native.PDPT = @ptrCast(
+            try pmm.allocPage(),
+        );
+        new_pml4e.setAddress(
+            pmm.virtualToPhysical(.{
+                .value = @intFromPtr(new_pdpt),
+            }),
+        );
+
+        new_pdpt.* = .{};
+        for (pdpt.entries, &new_pdpt.entries) |pdpte, *new_pdpte| {
+            if (!pdpte.present) {
+                continue;
+            }
+
+            new_pdpte.* = pdpte;
+            if (pdpte.leaf) {
+                continue;
+            }
+
+            const pdt_address = pdpte.getTableAddress();
+
+            const pdt: *align(std.mem.page_size) native.PDT = @ptrFromInt(
+                pmm.physicalToVirtual(pdt_address).value,
+            );
+
+            const new_pdt: *align(std.mem.page_size) native.PDT = @ptrCast(
+                try pmm.allocPage(),
+            );
+            new_pdpte.setTableAddress(
+                pmm.virtualToPhysical(.{
+                    .value = @intFromPtr(new_pdt),
+                }),
+            );
+
+            new_pdt.* = .{};
+            for (pdt.entries, &new_pdt.entries) |pdte, *new_pdte| {
+                if (!pdte.present) {
+                    continue;
+                }
+
+                new_pdte.* = pdte;
+                if (pdte.leaf) {
+                    continue;
+                }
+
+                const pt_address = pdte.getTableAddress();
+
+                const pt: *align(std.mem.page_size) native.PT = @ptrFromInt(
+                    pmm.physicalToVirtual(pt_address).value,
+                );
+
+                const new_pt: *align(std.mem.page_size) native.PT = @ptrCast(
+                    try pmm.allocPage(),
+                );
+                new_pdte.setTableAddress(
+                    pmm.virtualToPhysical(.{
+                        .value = @intFromPtr(new_pt),
+                    }),
+                );
+
+                @memcpy(&new_pt.entries, &pt.entries);
+            }
+        }
+    }
+
+    return new_page_table;
 }
 
 fn getPageTableAddress() mem.PhysicalAddress {
